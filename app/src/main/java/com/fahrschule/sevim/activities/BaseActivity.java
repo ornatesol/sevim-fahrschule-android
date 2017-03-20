f65c8d4f2b20f32910909ca534bd4427b65967f7
Wahib <wahib.tech@gmail.com>
2017-03-21 00:25:39 +0100

    Implementation for Demo 1
    - Internet Connection error
    - Nav items updated, LeraningSite and OfficeTiming added
    - Generic Activity added
    - Utils class added
    - All relevant Strings added

--- app/src/main/java/com/fahrschule/sevim/activities/BaseActivity.java
+++ app/src/main/java/com/fahrschule/sevim/activities/BaseActivity.java
@@ -1,9 +1,12 @@
 package com.fahrschule.sevim.activities;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +17,7 @@ import android.view.MenuItem;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.fahrschule.sevim.R;
+import com.fahrschule.sevim.utils.Utils;
 
 public abstract class BaseActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -36,9 +40,12 @@ public abstract class BaseActivity extends AppCompatActivity
         setupToolbar();
         setupDrawer();
         setupNavigationView();
-
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             showDefaultFragment();
+        }
+
+        if(!Utils.isOnline(this)) {
+            Utils.showConnectionErrorDialog(this, getString(R.string.error_no_connection));
         }
     }
 
@@ -63,23 +70,24 @@ public abstract class BaseActivity extends AppCompatActivity
     protected abstract void showDefaultFragment();
 
 
-    @SuppressWarnings("StatementWithEmptyBody") @Override
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_theory) {
-
+        if (id == R.id.nav_learning_site) {
+            Utils.openUrlInBrowser(this, getString(R.string.learning_site_url));
         } else if (id == R.id.nav_messages) {
 
-        } else if (id == R.id.nav_offers) {
+        } else if (id == R.id.nav_infos) {
 
-        } else if (id == R.id.nav_team) {
+        } else if (id == R.id.nav_locations) {
 
-        } else if (id == R.id.nav_opening_timings) {
+        } else if (id == R.id.nav_theory_calendar) {
 
-        } else if (id == R.id.nav_app_launch) {
-
+        } else if (id == R.id.nav_office_timings) {
+            startActivity(MainActivity.newIntent(this, getString(R.string.office_timing)));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -94,25 +102,5 @@ public abstract class BaseActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/fahrschule/sevim/activities/MainActivity.java
+++ app/src/main/java/com/fahrschule/sevim/activities/MainActivity.java
@@ -1,16 +1,48 @@
 package com.fahrschule.sevim.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import com.fahrschule.sevim.R;
 import com.fahrschule.sevim.fragments.MainFragment;
 
 public class MainActivity extends BaseActivity {
 
+    private static final String EXTRA_DESTINATION_TYPE = "args_destination_type";
+
+    public static Intent newIntent(final Context context) {
+        return new Intent(context, MainActivity.class);
+    }
+
+    public static Intent newIntent(final Context context, String destinationType) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(EXTRA_DESTINATION_TYPE, destinationType);
+        return intent;
+    }
+
     @Override
     protected void showDefaultFragment() {
-        Fragment fragment = MainFragment.newInstance();
+        decideDestination(getIntent());
+    }
+
+    private void showOfficeTimingContent() {
+        String timingText = getString(R.string.office_timing_value);
+        Fragment fragment = MainFragment.newInstance(timingText);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content, fragment)
                 .commit();
+    }
+
+    private void decideDestination(Intent intent) {
+        if(intent.hasExtra(EXTRA_DESTINATION_TYPE)) {
+            if(intent.getStringExtra(EXTRA_DESTINATION_TYPE).equals(
+                    getString(R.string.office_timing))) {
+                showOfficeTimingContent();
+            }
+        } else {
+            //default case
+            Fragment fragment = MainFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
+        }
     }
 }
