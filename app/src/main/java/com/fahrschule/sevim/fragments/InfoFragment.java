518f7de6a63ccc94fc9076c1c851615f08dd18ca
Wahib-Ul-Haq <wahib.tech@gmail.com>
2017-04-20 23:07:58 +0200

    Infos content added instead of image

--- app/src/main/java/com/fahrschule/sevim/fragments/InfoFragment.java
+++ app/src/main/java/com/fahrschule/sevim/fragments/InfoFragment.java
@@ -16,6 +16,6 @@ public class InfoFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.view_services_imageview, container, false);
+        return inflater.inflate(R.layout.fragment_infos, container, false);
     }
 }
